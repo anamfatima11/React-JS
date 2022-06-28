ccc56c56d481b63de375ec035d8b020d449e79a0
anamfatima11 <97897306+anamfatima11@users.noreply.github.com>
2022-06-28 08:37:51 +0530

    Add files via upload

---
+++ UserInfoByClass.js
@@ -0,0 +1,203 @@
+import React from "react";
+
+class UserInfoByClass extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            username: 'Anam', 
+            email: 'anam@tekisky.com',
+            phonenumber: '9289994397'
+        }
+    }
+    render(){
+        return(
+        <>
+            <p>This is Class Component</p>
+            <p>username{this.state.username}</p>
+            <input id='myInput1' onChange={(event) => {
+                let changeUserName = event.target.value;
+                this.setState({username:changeUserName})
+            }}></input>
+
+ <p> Email{ this.state.email}</p>
+ <input id='myInput2' onChange={(event) => {
+    let changeEmailid = event.target.value;
+    this.setState({email:changeEmailid})
+            }}>
+            </input>
+            <p>PhoneNumber {this.state.phonenumber}</p>
+            <input id='myInput3' onChange={(event) =>{
+            let changePhoneNumber = event.target.value;
+            this.setState({phonenumber:changePhoneNumber})
+        }}>
+            </input>
+         </>
+        )
+    }
+}
+export default UserInfoByClass;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
